5af4c3813edb6314bb7f26b45f6a9e91f963b8a3
KnightOfMelons <ilaprosmotrov@gmail.com>
2024-11-12 15:02:29 +0300

    Modelling part added

--- main.py
+++ main.py
@@ -2,24 +2,41 @@ import keras
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-# Setting seed for reproducability
-np.random.seed(1234)
-PYTHONHASHSEED = 0
 from sklearn import preprocessing
 from sklearn.metrics import confusion_matrix, recall_score, precision_score
 # from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM, Activation
+# Setting seed for reproducability
+np.random.seed(1234)
+PYTHONHASHSEED = 0
+
+# ==== Data Ingestion ====
+# В следующем разделе мы принимаем обучающие, тестовые и достоверные наборы данных из хранилища Azure.
 
 # read training data
 train_df = pd.read_csv('DOWNLOAD_FILES/PM_train.txt', sep=" ", header=None)
 train_df.drop(train_df.columns[[26, 27]], axis=1, inplace=True)
 train_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
-                     's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
-                     's15', 's16', 's17', 's18', 's19', 's20', 's21']
+                    's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
+                    's15', 's16', 's17', 's18', 's19', 's20', 's21']
 
-train_df = train_df.sort_values(['id','cycle'])
+# read test data
+test_df = pd.read_csv('DOWNLOAD_FILES/PM_test.txt', sep=" ", header=None)
+test_df.drop(test_df.columns[[26, 27]], axis=1, inplace=True)
+test_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
+                   's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
+                   's15', 's16', 's17', 's18', 's19', 's20', 's21']
+
+# read ground truth data
+truth_df = pd.read_csv('DOWNLOAD_FILES/PM_truth.txt', sep=" ", header=None)
+truth_df.drop(truth_df.columns[[1]], axis=1, inplace=True)
+
+train_df = train_df.sort_values(['id', 'cycle'])
 print("\n==== Data Ingestion ====\n\n", train_df.head())
+
+# ==== Data Preprocessing ====
+# Первым шагом является создание меток для обучающих данных: «Оставшийся срок полезного использования» (RUL),
+# label1 и label2, как это было сделано в шаблоне прогнозируемого обслуживания.
 
 # Data Labeling - generate column RUL
 rul = pd.DataFrame(train_df.groupby('id')['cycle'].max()).reset_index()
@@ -28,3 +45,79 @@ train_df = train_df.merge(rul, on=['id'], how='left')
 train_df['RUL'] = train_df['max'] - train_df['cycle']
 train_df.drop('max', axis=1, inplace=True)
 print("\n==== Data Preprocessing ====\n\n", train_df.head())
+
+# generate label columns for training data
+w1 = 30
+w0 = 15
+train_df['label1'] = np.where(train_df['RUL'] <= w1, 1, 0)
+train_df['label2'] = train_df['label1']
+train_df.loc[train_df['RUL'] <= w0, 'label2'] = 2
+print(train_df.head())
+
+# Предполагается, что train_df уже загружен и содержит необходимые столбцы
+train_df['cycle_norm'] = train_df['cycle']
+cols_normalize = train_df.columns.difference(['id', 'cycle', 'RUL', 'label1', 'label2'])
+min_max_scaler = preprocessing.MinMaxScaler()
+norm_train_df = pd.DataFrame(min_max_scaler.fit_transform(train_df[cols_normalize]),
+                             columns=cols_normalize,
+                             index=train_df.index)
+join_df = train_df[train_df.columns.difference(cols_normalize)].join(norm_train_df)
+train_df = join_df.reindex(columns=train_df.columns)
+print(train_df.head())
+
+test_df['cycle_norm'] = test_df['cycle']
+cols_normalize = test_df.columns.difference(['id', 'cycle', 'RUL', 'label1', 'label2'])
+norm_test_df = pd.DataFrame(min_max_scaler.transform(test_df[cols_normalize]),
+                            columns=cols_normalize,
+                            index=test_df.index)
+test_join_df = test_df[test_df.columns.difference(cols_normalize)].join(norm_test_df)
+test_df = test_join_df.reindex(columns=test_df.columns)
+test_df = test_df.reset_index(drop=True)
+print(test_df.head())
+
+# generate column max for test data
+rul = pd.DataFrame(test_df.groupby('id')['cycle'].max()).reset_index()
+rul.columns = ['id', 'max']
+truth_df.columns = ['more']
+truth_df['id'] = truth_df.index + 1
+truth_df['max'] = rul['max'] + truth_df['more']
+truth_df.drop('more', axis=1, inplace=True)
+
+# generate RUL for test data
+test_df = test_df.merge(truth_df, on=['id'], how='left')
+test_df['RUL'] = test_df['max'] - test_df['cycle']
+test_df.drop('max', axis=1, inplace=True)
+print(test_df.head())
+
+# generate label columns w0 and w1 for test data
+test_df['label1'] = np.where(test_df['RUL'] <= w1, 1, 0)
+test_df['label2'] = test_df['label1']
+test_df.loc[test_df['RUL'] <= w0, 'label2'] = 2
+print(test_df.head())
+
+# ==== Modelling ====
+
+# pick a large window size of 50 cycles
+sequence_length = 50
+
+# Давайте сначала посмотрим на пример значений датчика за 50 циклов до сбоя для идентификатора двигателя 3.
+# Мы будем передавать в сеть LSTM данные этого типа для каждого временного шага для каждого идентификатора двигателя.
+# preparing data for visualizations
+# window of 50 cycles prior to a failure point for engine id 3
+engine_id3 = test_df[test_df['id'] == 3]
+engine_id3_50cycleWindow = engine_id3[engine_id3['RUL'] <= engine_id3['RUL'].min() + 50]
+cols1 = ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10']
+engine_id3_50cycleWindow1 = engine_id3_50cycleWindow[cols1]
+cols2 = ['s11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+engine_id3_50cycleWindow2 = engine_id3_50cycleWindow[cols2]
+
+# !!!!! Визуализация данных сенсоров для двигателя ID 3 перед точкой отказа - сенсоры 1-10 !!!!!
+ax1 = engine_id3_50cycleWindow1.plot(subplots=True, sharex=True, figsize=(20, 20))
+
+# Отображение графиков
+plt.show()
+
+
+# plotting sensor data for engine ID 3 prior to a failure point - sensors 11-21
+ax2 = engine_id3_50cycleWindow2.plot(subplots=True, sharex=True, figsize=(20,20))
+plt.show()
